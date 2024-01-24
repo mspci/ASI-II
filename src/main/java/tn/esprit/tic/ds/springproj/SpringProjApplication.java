7a076f16240d5fb4ecdc9d528a0515d107de8030
mspci <wint3rmorning@gmail.com>
2024-01-24 21:42:44 +0100

    Initial commit

---
+++ src/main/java/tn/esprit/tic/ds/springproj/SpringProjApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.tic.ds.springproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjApplication.class, args);
+    }
+
+}
