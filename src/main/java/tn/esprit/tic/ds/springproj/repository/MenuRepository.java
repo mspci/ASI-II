f2da803178e532624e128c8fe5c3674aa101e49c
mspci <wint3rmorning@gmail.com>
2024-02-24 00:31:35 +0100

    S5 JPQL

--- src/main/java/tn/esprit/tic/ds/springproj/repository/MenuRepository.java
+++ src/main/java/tn/esprit/tic/ds/springproj/repository/MenuRepository.java
@@ -1,9 +1,24 @@
 package tn.esprit.tic.ds.springproj.repository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import tn.esprit.tic.ds.springproj.entities.Menu;
+import tn.esprit.tic.ds.springproj.entities.TypeComposant;
+import tn.esprit.tic.ds.springproj.entities.TypeMenu;
 
 import java.util.List;
 
 public interface MenuRepository {
     List<Menu> findAllByComposantsOrderByPrixTotalGreaterThan(float PrixTotal);
+
+    @Query("SELECT m.libelleMenu FROM Menu m " +
+            "WHERE m.typeMenu = :typeMenu " +
+            "ORDER BY m.prixTotal")
+    List<String> retrieveMenuLabelByMenuTypeOrderedByPrice(@Param("typeMenu") TypeMenu typeMenu);
+
+    @Query("SELECT m FROM Menu m " +
+            "JOIN m.composants c " +
+            "JOIN c.detailComposant dc " +
+            "WHERE dc.typeComposant = :typeComposant")
+    List<Menu> retrieveMenuByTypeComposant(@Param("typeComposant") TypeComposant typeComposant);
 }
